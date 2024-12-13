caa41e94c7e7f868186fb666cf879b6ad202c145
NirtoZeus99 <orion_guzman@student.davincischools.org>
2024-12-13 11:39:32 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// use variables to make your code shorter
+var a;
+var b;
+var c;
+var d;
+
+// declare your functions
+function firstLogo() {
+  document.getElementById("logo").classList.add("cant-see-me");
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/DZ9sUTqyYGM?si=iZFi0OmjY699uiLw&amp&start=0?&autoplay=1";
+  var video = document.getElementById("video");
+  video.style.display = "block"; // Change the display style to block
+}
+//https://www.youtube.com/embed/DZ9sUTqyYGM?si=iZFi0OmjY699uiLw&amp&start=5?&autoplay=1
+
+function secondLogo() {
+  document.getElementById("logo").classList.add("cant-see-me");
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/u0cPWB-HVVo?si=Q6JWz8xiVe8I416p&start=0?&autoplay=1";
+  var video = document.getElementById("video");
+  video.style.display = "block"; // Change the display style to block
+}
+
+function thirdLogo() {
+  document.getElementById("logo").classList.add("cant-see-me");
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/ZDDMCd_wNNw?si=-dtUB8YOiG00CR4G&start=0?&autoplay=1";
+  var video = document.getElementById("video");
+  video.style.display = "block"; // Change the display style to block
+}
+
+function fourthLogo() {
+  document.getElementById("logo").classList.add("cant-see-me");
+  document.getElementById("video").src =
+    "https://www.youtube.com/embed/KllvvjcRR0A?si=7d0UQcK83gk0nmJ0&start=0?&autoplay=1";
+  var video = document.getElementById("video");
+  video.style.display = "block"; // Change the display style to block
+}
